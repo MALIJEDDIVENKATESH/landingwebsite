cc4805db012f719319c7165ba293065b129de555
MALIJEDDIVENKATESH <malijeddivenkatesh19062001@gmail.com>
2021-04-05 22:21:31 +0530

    added testimonials and ui changes

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 
 
-const submit=()=>{
+function popup(){
     alert("Submitted successfully");
 }
 
+
+scrollbutton=document.getElementById('scrollbutton')
+window.onscroll = function() {
+    scrollfunction()
+};
+function scrollfunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      scrollbutton.style.display = "block";
+    } else {
+      scrollbutton.style.display = "none";
+    }
+  }
+  
+  function gototop() {
+    document.documentElement.scrollTop = 0; 
+  }
